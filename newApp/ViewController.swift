d5f2d2ee53fe1b7563c61c68bb61a852e0cf1f79
Aleksa <aleksa.khruleva@gmail.com>
2020-06-10 10:46:26 +0300

    Initial Commit

---
+++ newApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newApp
+//
+//  Created by Aleksa on 10.06.2020.
+//  Copyright © 2020 Aleksa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
